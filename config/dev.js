02951cf5a3c70a0b3678918686327be9ba815b51
Derek Garnett <drkgrntt@gmail.com>
2017-12-30 14:52:53 -0600

    nothing...

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  adminCode: 'admincode',
-  databaseURL: 'mongodb://localhost/blog_app',
-  emailPassword: 'wrongpassword',
-  cookieKey: 'xedcrfvgtbhynjumkiEXDCRFVGBNJUMswxedcfrvgbthnjum',
-  cloudinaryKey: '249443357945341',
-  cloudinarySecret: 'JnZPF3e9n2Jc-TUps7sYK1gj5pU'
-};
